a11b43997a5466d74f676dfa2056eb1c77d2fe4a
captcha781 <bhuvanesh19112001@gmail.com>
2025-08-09 19:10:29 +0530

    AGENT - Updated config

--- agent-service/backend/config/main.py
+++ agent-service/backend/config/main.py
@@ -15,15 +15,6 @@ CORS_ORIGIN = config.get("CORS_ORIGIN", default="*")
 MONGO_URI = config.get("MONGO_URI")
 PORT = config.get("PORT", default=5000, cast=int)
 
-JWT_ACCESS_EXPIRY = parse_timespan(config.get("JWT_ACCESS_EXPIRY"))
-JWT_REFRESH_EXPIRY = parse_timespan(config.get("JWT_REFRESH_EXPIRY"))
-JWT_ACCESS_KEY = config.get("JWT_ACCESS_KEY", cast=str)
-JWT_REFRESH_KEY = config.get("JWT_REFRESH_KEY", cast=str)
-
-SECRET_OR_KEY = config.get("SECRET_OR_KEY", cast=str)
-
-STORE_DIR = os.path.join(os.path.dirname(__file__), "..", "store")
-
 ASTRA_DB_SECRET_KEY = config.get("ASTRA_DB_SECRET_KEY", cast=str)
 ASTRA_DB_ENDPOINT = config.get("ASTRA_DB_ENDPOINT", cast=str)
 ASTRA_DB_KEYSPACE = config.get("ASTRA_DB_KEYSPACE", cast=str)
