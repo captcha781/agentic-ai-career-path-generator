acf333cd4749d94bea1e90a6e43e6dde0f0e2205
captcha781 <bhuvanesh19112001@gmail.com>
2025-08-09 14:37:25 +0530

    FRONT - Dependancy & Configs

---
+++ frontend/src/config/index.js
@@ -0,0 +1,8 @@
+const config = {
+  SITE_NAME: "MedScore",
+  MODE: import.meta.env.VITE_MODE,
+  USER_URL: import.meta.env.VITE_USER_URL,
+  AGENT_URL: import.meta.env.VITE_AGENT_URL,
+};
+
+export default config;
