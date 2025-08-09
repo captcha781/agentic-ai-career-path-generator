864276e6a793b5b6f76388decf3516a959bacff1
captcha781 <bhuvanesh19112001@gmail.com>
2025-08-09 20:28:00 +0530

    USER - Internal route fix

--- user-service/routes/index.js
+++ user-service/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import authRoutes from './auth'
 import userRoutes from './user'
+import intrRoutes from './internal'
 
 const router = express.Router()
 
 router.use('/auth', authRoutes)
 router.use('/user', userRoutes)
+router.use('/internal', intrRoutes)
 
 export default router
