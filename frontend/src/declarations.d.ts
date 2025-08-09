b5b08fe274f5bac0ece30938f950e7bb7adafa31
captcha781 <bhuvanesh19112001@gmail.com>
2025-08-09 14:39:55 +0530

    FRONT - Added type declarations for Toast

---
+++ frontend/src/declarations.d.ts
@@ -0,0 +1,8 @@
+// declarations.d.ts or svg.d.ts
+declare module '*.svg?react' {
+  import * as React from 'react';
+  const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+  export default ReactComponent;
+}
